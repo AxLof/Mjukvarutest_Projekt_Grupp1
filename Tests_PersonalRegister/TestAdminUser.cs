f74a4af1bd0473cb34fe5dfa46866f26ea21e51f
Axel Löfgren <h23axlof@du.se>
2025-05-26 21:09:44 +0200

    Ett till test, nu rullar vi.

--- Tests_PersonalRegister/TestAdminUser.cs
+++ Tests_PersonalRegister/TestAdminUser.cs
@@ -6,12 +6,12 @@ using static System.Runtime.InteropServices.JavaScript.JSType;
 
 namespace Tests_PersonalRegister
 {
-    public class UnitTest1
+    public class TestAdminUser
     {
         [Fact]
         public void LoadEmployeesFromDatabase_LoadsAndReturnsCorrectEmployees()
         {
-            // Arrange
+            
             DataTable testTable = new DataTable();
             testTable.Columns.Add("UniqueID", typeof(string));
             testTable.Columns.Add("Role", typeof(string));   
@@ -53,9 +53,29 @@ namespace Tests_PersonalRegister
             
             Assert.Equal(listResult, expectedListResult);
             Assert.Equal(dictResult, expectedDictResult);
+        }
 
+        [Fact]
+        public void AddEmployee_AddsCorrectEmployee()
+        {
+            DataTable testTable = new DataTable();
+            testTable.Columns.Add("UniqueID", typeof(string));
+            testTable.Columns.Add("Role", typeof(string));
 
+            var mockDatabaseHelper = new Mock<IDatabaseHelper>();
+            mockDatabaseHelper.Setup(db => db.ExecuteQuery("SELECT UniqueID, Role FROM Users"))
+                .Returns(testTable);
+            mockDatabaseHelper.Setup(db => db.ExecuteNonQuery("INSERT INTO Users (UniqueID, Role) " +
+                                                              "VALUES (@UniqueID, @Role)"));
+                
+            AdminUser testUser = new AdminUser(mockDatabaseHelper.Object);
+            testUser.AddEmployee("TEST123", "APA");
+            var employeeList = testUser.GetAllEmployeesList();
+            var employeeDict = testUser.GetAllEmployeesDictionary();
 
+            Assert.Contains("TEST123: APA", employeeList);
+            Assert.Contains("TEST123", employeeDict.Keys);
+            Assert.Contains("APA", employeeDict.Values);
         }
     }
 }
